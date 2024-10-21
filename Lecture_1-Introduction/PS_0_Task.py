3acf21420f77c2a339859f92a410bf87b4d76947
4rty2u <https://github.com/4rty2u>
2024-10-21 20:49:52 -0300

    Task that was assigned in ps0

---
+++ Lecture_1-Introduction/PS_0_Task.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 20:47:00 2024
+
+@author: mi
+"""
+
+import math
+
+x = int(input("Type a number: "))
+y = int(input("Type another number: "))
+
+print(x**y)
+print(math.log(x, 2))
